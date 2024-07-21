64b17c8575729908fc32711262af7a45e0248e3b
DestinyH04 <dhopson7@students.kennesaw.edu>
2024-07-20 21:59:32 -0400

    Update Hotel-Management-Tracker.py
    Added "booking" so guests can book through system as well. Much more work to go!

--- Hotel-Management-Tracker.py
+++ Hotel-Management-Tracker.py
@@ -1,27 +1,36 @@
-attempts = 0
+#This is a coding system for hotel management from the guests' UI/UX 
+
+#Hotel menus
 brunch_menu = ["Pancakes", "Shrimp & Grits", "All-Star Breakfast", "Omelette"]
 lunch_menu = ["Cesar Salad", "Club Snadwich", "1/3 lb. Beef Burger w/ Fries", "Fish Tacos"]
 dinner_menu = ["Steak & Frites", "Fish & Chips", "1/3 lb. Beef Burger w/ Fries", "Short Ribs with Mashed Potatoes"]
 appetizer_menu = ["Spinach & Artichoke Dip", "Pretzel Bites", "Garden Salad", "Hummus"]
+
+#counter to keep track of rooms/lists
 menu = []
 rooms = []
+attempts = 0
 rooms_taken = 0
 single_avail = 100
 double_avail = 75
 kings_avail = 50
 suites_avail  = 25
+date = []
+
+
 #Opulent Oasis offers four types of rooms: Single Queen, Double Queen, King, and Suites
 #Use "guest_name" for username and "guest_room" for password
 guest_name = input("Please enter your first name and last intial: ")
 guest_room = input("Please enter your room number: ")
 
+#login
 user_name = input("Enter username: ")
 password = input("Enter password: ")
    
 for entry in range(3):
     
     if user_name == guest_name and password == guest_room:
-        access = input("Access Granted! Main Menu ''\n'' Enter "" 1"" for Check-in/out'\n' Enter ""2"" for Billing '\n' Enter ""3"" for Room Service: ")
+        access = input("Access Granted! Main Menu ''\n'' Enter "" 1"" for Check-in/out'\n' Enter ""2"" for Billing '\n' Enter ""3"" for Room Service '\n' Enter ""4"" for Booking: ")
         attempts += 1
         break
     else:
@@ -69,26 +78,38 @@ if menu.capitalize == "B":
 
 if menu.capitalize == "L":
                 print(lunch_menu)
+                room = input("Please enter the room number you are currently staying in: ")
                 order_lunch = input("What would the guest like to order?")
                 if order_lunch is in lunch_menu:
-                    print("Thank you",guest_name, "! Your order:" ,order_lunch, "has been placed with an estimated arrival time of 15 minutes.")
+                    print("Thank you",guest_name, "! Your order:" ,order_lunch, "has been placed with an estimated arrival time of 15 minutes. You have been billed for your order")
 
 if menu.capitalize == "D":
                 print(dinner_menu)
                 order_dinner = input("What would the guest like to order?")
                 if order_dinner is in dinner_menu:
-                    print("Thank you",guest_name, "! Your order:" ,order_dinner, "has been placed with an estimated arrival time of 15 minutes.") 
+                    print("Thank you",guest_name, "! Your order:" ,order_dinner, "has been placed with an estimated arrival time of 15 minutes. You have been billed for your order.") 
         
 if menu.capitalize == "D":
                 print(dinner_menu)
                 order_dinner = input("What would the guest like to order?")
                 if order_dinner is in dinner_menu:
-                    print("Thank you",guest_name, "! Your order:" ,order_dinner, "has been placed with an estimated arrival time of 15 minutes.") 
+                    print("Thank you",guest_name, "! Your order:" ,order_dinner, "has been placed with an estimated arrival time of 15 minutes. You have been billed for your order.") 
         
 if menu.capitalize == "A":
                 print(dinner_menu)
                 order_app = input("What would the guest like to order?")
                 if order_app is in appetizer_menu_menu:
-                    print("Thank you",guest_name, "! Your order:" ,order_app, "has been placed with an estimated arrival time of 15 minutes.") 
+                    print("Thank you",guest_name, "! Your order:" ,order_app, "has been placed with an estimated arrival time of 15 minutes. You have been billed for your order") 
+
+if access == "4" 
+   date = input("Thank you for choosing to stay with Opulent Oasis. When would you check-in? (MM/DD/YY"))
+   date_2 = input("Thank you for choosing to stay with Opulent Oasis. When would you check-out? (MM/DD/YY"))
+   booking = input("Which type of room would you like to book? Enter Single, Double, King, or Suite")
+      if booking == "Single"
+         print("Thank you! You have booked with Opulent Oasis on:" , date, "to", date_2, ". We look forward to seeing you soon!")
+         date = date.append()
+         date_2 = date.append()
+
+         
         
         
